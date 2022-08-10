89812b2bca78d8db63baae0b7f2549e679d0d030
david riyan kurniawan <103322177+davidriyan@users.noreply.github.com>
2022-08-10 11:52:38 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+  void tekantombol() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("data"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: number.toDouble() + 10),
+              ),
+              ElevatedButton(onPressed: tekantombol, child: Text("data"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
